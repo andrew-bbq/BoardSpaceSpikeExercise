3b863bff67786b84e02c6572aad71dc98560451d
Aksel Balkir <abalkir@wisc.edu>
2021-02-24 22:40:11 -0600

    usage report

--- routes/staff.js
+++ routes/staff.js
@@ -37,6 +37,99 @@ router.post('/query',function (req, res, next) {
     });
 });
 
+router.get('/usagereport', function (req, res, next) {
+    // only admins and staff can view this page
+    if (!req.session.user || (req.session.user.role != User.schema.path('role').enumValues[0] && req.session.user.role != User.schema.path('role').enumValues[2])) {
+        return res.redirect('/');
+    }
+    let orderCount = 0;
+
+    var query = Order.find(); 
+
+    query.count(function (err, count) { 
+        if (err) return next(err)
+        orderCount = count; 
+    });
+
+    Order.find({}, function (err, orders) {
+        if (err) return next(err)
+        MenuItem.find({}, function (err, menuItems) {
+            if (err) return next(err)
+            return res.render('usagereport', { orders: orders, orderCount: orderCount,itemOrderCount: 0, itemCount: 0, itemTotalPaid: 0, menuItems: menuItems});
+        });
+    });
+});
+
+router.post('/getUsageReport',function (req, res, next) {
+    // only admins and staff can view this page
+    if (!req.session.user || (req.session.user.role != User.schema.path('role').enumValues[0] && req.session.user.role != User.schema.path('role').enumValues[2])) {
+        return res.redirect('/');
+    }
+    let queryItemCount = 0;
+    let orderCount = 0;
+    let itemOrderCount = 0;
+    let itemTotalPaid = 0.0;
+
+    let queryItem = req.body.queryItem;
+    let startdate = req.body.queryStartDate;
+    let enddate = req.body.queryEndDate;
+
+
+    // getting count of orders
+    var query = Order.find({/***/ time: {$gte: startdate, $lte: enddate} /***/}); 
+
+    query.count(function (err, count) { 
+        if (err) return next(err); 
+        else {
+            orderCount = count; 
+        }
+    }); 
+
+    // getting id of selected menu item
+
+    // let queryItemId;
+
+    // MenuItem.findOne({name:queryItem}, function (err, menuItem) {
+    //     if (err) return next(err); 
+    //     queryItemId = menuItem.id;
+    // });
+
+    // MenuItem.find({name:queryItem}, function (err, menuItems) {
+    //     menuItems.forEach(function(menuItem) {
+    //         if (err) return next(err); 
+    //         if(menuItem.name === queryItem) queryItemId = menuItem.id;
+    //     });
+    // });
+
+    // getting count of selected menu item
+    Order.find({/***/ time: {$gte: startdate, $lte: enddate} /***/}, function(err, orders) {
+        if (err) return next(err);
+        orders.forEach(function(order) {
+            if (err) return next(err);
+            let menuIds = [];
+            for (let itemId in order.menuItems) menuIds.push(itemId); 
+            MenuItem.find({ '_id': {$in: menuIds}, name:queryItem}, function(err, menuItems) {
+                if (err) return next(err);
+                for (let menuId in order.menuItems) {
+                    menuItems.forEach(function(menuItem) {
+                        if (menuId == menuItem.id) {
+                            queryItemCount += order.menuItems[menuId];
+                            itemOrderCount++;
+                            itemTotalPaid += Number(menuItem.price)*Number(order.menuItems[menuId]).toFixed(2);
+                        }
+                    });
+                }
+            });
+        });
+    });
+
+    Order.find({/***/ time: {$gte: startdate, $lte: enddate} /***/}, function (err, orders) {
+        MenuItem.find({},function (err, menuItems) {
+            return res.render('usagereport', { orders: orders, orderCount: orderCount, itemOrderCount: itemOrderCount, itemCount: queryItemCount, itemTotalPaid: itemTotalPaid.toFixed(2), menuItems: menuItems});
+        });
+    });
+});
+
 router.get('/vieworder', function(req, res, next) {
     if (!req.session.user || (req.session.user.role != User.schema.path('role').enumValues[0] && req.session.user.role != User.schema.path('role').enumValues[2])) {
         return res.redirect('/');
